5f776b027100b2fb25be9cd7ee7c1a69da305865
Arif Enes Aydın <aydnarifenes@gmail.com>
2024-05-12 22:16:04 +0300

    Added comments

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 This script is required for [*qupath-extension-tseg*](https://github.com/ae-aydin/qupath-extension-tseg) extension.
 
-Gets predictions on given ROI, converts and saves predictions as GeoJSON format.
+Gets predictions on given ROI as tiles, converts and saves predictions as GeoJSON format.
 
 For usage check [*qupath-extension-tseg*](https://github.com/ae-aydin/qupath-extension-tseg).
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import re
 import time
 from pathlib import Path
-import torch
 
 import cv2
 import geojson
@@ -20,9 +19,20 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s",
     datefmt="%H:%M:%S",
 )
-     
+
 
 def add_offsets(contour: list, offset_x: float, offset_y: float):
+    """
+    Add ROI offset values to coordinates.
+
+    Args:
+        contour (list): Found polygons and coordinates.
+        offset_x (float): Offset value in X-axis.
+        offset_y (float): Offset value in Y-axis.
+
+    Returns:
+        list: Offset added coordinates.
+    """
     np_contour = np.array(contour)
     np_contour[:, 0] = np_contour[:, 0] + offset_x
     np_contour[:, 1] = np_contour[:, 1] + offset_y
@@ -32,6 +42,18 @@ def add_offsets(contour: list, offset_x: float, offset_y: float):
 def mask_to_geojson(
     mask: np.ndarray, output_path: str, args: argparse.Namespace, properties: dict
 ):
+    """
+    Extract polygons from given mask and save to given output path.
+
+    Args:
+        mask (np.ndarray): Segmentation mask.
+        output_path (str): Output path.
+        args (argparse.Namespace): Parsed command-line arguments.
+        properties (dict): QuPath-specific geoJson properties.
+
+    Returns:
+        None
+    """
     contours, hierarchy = cv2.findContours(
         mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS
     )
@@ -61,20 +83,48 @@ def mask_to_geojson(
 
 
 def get_boundaries(tile_info: dict, args: argparse.Namespace, mask_shape: tuple):
+    """
+    Get slice boundaries of to-be-assigned mask to main mask.
+
+    Args:
+        tile_info (dict): Tile properties.
+        args (argparse.Namespace): Parsed command-line arguments.
+        mask_shape (tuple): Exact sizes of the mask.
+
+    Returns:
+        tuple: A tuple containing tile properties.
+            - lower_y (int): The lower bound of the y-coordinate.
+            - upper_y (int): The upper bound of the y-coordinate.
+            - lower_x (int): The lower bound of the x-coordinate.
+            - upper_x (int): The upper bound of the x-coordinate.
+    """
     lower_x = round((tile_info["x"] - args.roi_x) / args.ds)
     diff_x = abs(lower_x) if lower_x < 0 else 0
+
     lower_x = 0 if lower_x < 0 else lower_x
     upper_x = lower_x + mask_shape[1] + diff_x
+
     lower_y = round((tile_info["y"] - args.roi_y) / args.ds)
     diff_y = abs(lower_y) if lower_y < 0 else 0
+
     lower_y = 0 if lower_y < 0 else lower_y
     upper_y = lower_y + mask_shape[0] + diff_y
+
     assert upper_x - lower_x == mask_shape[1]
     assert upper_y - lower_y == mask_shape[0]
     return (lower_y, upper_y, lower_x, upper_x)
 
 
 def extract_tile_info(tile_name: str):
+    """
+    Extract tile information from tile file name.
+
+    Args:
+        tile_name (str): Name of the tile file.
+
+    Returns:
+        dict: Tile properties.
+    """
     pattern = r"\[(.*?)\]"
     matches = re.findall(pattern, tile_name)
     if matches:
@@ -91,6 +141,21 @@ def extract_tile_info(tile_name: str):
 
 
 def clean_mask(mask: np.ndarray, operation: str, kernel_size: int):
+    """
+    Apply a morphological operation to given mask.
+
+    Args:
+        mask (np.ndarray): The input mask.
+        operation (str): The type of morphological operation to apply.
+            Supported values: 'erosion', 'dilation', 'opening', 'closing'.
+        kernel_size (int): The size of the kernel used for morphological operations.
+
+    Raises:
+        ValueError: If the specified operation is not supported.
+
+    Returns:
+        np.ndarray: Morpohology applied mask.
+    """
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     if operation == "close":
         cleaned_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
@@ -105,19 +170,29 @@ def clean_mask(mask: np.ndarray, operation: str, kernel_size: int):
     return cleaned_mask
 
 
-def predict(
-    config: dict, args: argparse.Namespace, operation: str, k_size: int
-):
-    full_shape = (int(args.roi_width), int(args.roi_height))
-    logging.info(f"Selected region size is {format_shapes(full_shape)}")
+def predict(config: dict, args: argparse.Namespace, operation: str, k_size: int):
+    """
+    Get predictions on all tiles and reconstruct a mask for main ROI.
 
+    Args:
+        config (dict): Dictionary containing configurations such as I/O paths.
+        args (argparse.Namespace): Parsed command-line arguments.
+        operation (str): Morphological operation to use.
+        kernel_size (int): The size of the kernel used for morphological operations.
+
+    Returns:
+        np.ndarray: Reconstructed segmentation mask.
+    """
+    full_shape = (int(args.roi_height), int(args.roi_width))
     w = int(np.ceil(args.roi_width / args.ds))
     h = int(np.ceil(args.roi_height / args.ds))
-    logging.info(f"Downsampled region size is {format_shapes((w, h))}")
+    logging.info(f"ROI size: {format_shapes(full_shape)} -> {format_shapes((h, w))}")
 
     main_mask = np.zeros((h, w), dtype=np.uint8)
     model = YOLO(config["model_path"])
-    logging.info(f"Model arguments - confidence: {args.conf}, iou: {args.iou}, imgsz: {args.imgsz}")
+    logging.info(
+        f"Model arguments - confidence: {args.conf}, iou: {args.iou}, imgsz: {args.imgsz}"
+    )
     tile_count = len(os.listdir(config["roi_tiles_path"]))
     logging.info(f"{tile_count} tiles found.")
 
@@ -128,10 +203,11 @@ def predict(
         iou=args.iou,
         imgsz=args.imgsz,
         stream=True,
-        verbose=False
+        verbose=False,
     )
     logging.info(f"Inference completed in {time.time() - infer_start:.2f} seconds.")
 
+    logging.info("Arranging predictions to create full mask...")
     mask_start = time.time()
     empty_tile_list = list()
     for pred in preds:
@@ -153,7 +229,7 @@ def predict(
         )
         bound = get_boundaries(tile_info, args, merged_mask.shape)
         main_mask[bound[0] : bound[1], bound[2] : bound[3]] |= merged_mask
-    logging.info(f"Mask prepared in {time.time() - mask_start:.2f} seconds.")
+    logging.info(f"Full mask prepared in {time.time() - mask_start:.2f} seconds.")
 
     if not np.any(empty_tile_list):
         logging.warning("NO OBJECTS FOUND.")
@@ -167,6 +243,12 @@ def predict(
 
 
 def load_arguments():
+    """
+    Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed command-line arguments.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("base_path", type=Path)
     parser.add_argument("roi_x", type=float)
