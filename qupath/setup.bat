40d996506cc77d175dbc8f091844597e671cda06
ae-aydin <aydnarifenes@gmail.com>
2025-07-20 22:30:00 +0300

    Add placeholder setup script for testing

---
+++ qupath/setup.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Hello from setup.bat!
+echo Current directory: %cd%
+echo Listing files:
+dir
+exit /b 0
---
+++ qupath/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello from setup.sh!"
+echo "Current directory: $(pwd)"
+echo "Listing files:"
+ls -l
+exit 0
