2fa61fcaae6d70999dcc53d411f4d91bcc69a7d7
ae-aydin <aydnarifenes@gmail.com>
2025-11-13 00:00:06 +0300

    Fix argparse syntax error

--- infer.py
+++ infer.py
@@ -58,7 +58,7 @@ def main():
     logger = None
 
     try:
-        logger = setup_logger(args.log, debug=True)
+        logger = setup_logger(args.log_file, debug=True)
         logger.info(f"Arguments: {vars(args)}")
 
         start_time = time.perf_counter()
